cdcc9e418b19256bf7253a78d332756fa2b1c298
Ousama Elayan <ousamasama@gmail.com>
2018-10-17 21:27:23 -0500

    added first advanced challenge

--- book3chapter6/main.js
+++ book3chapter6/main.js
@@ -73,41 +73,102 @@ const students = [
     }
 ]
 
-const section = (title, style) => {
-    return `<section class="bordered dashed ${style}">${title}</section>`
-}
+// const section = (title, style) => {
+//     return `<section class="bordered dashed ${style}">${title}</section>`
+// }
 
-const aside = (title, style) => {
-    return `<aside class="${style}">${title}</aside>`
-}
+// const aside = (title, style) => {
+//     return `<aside class="${style}">${title}</aside>`
+// }
 
-const h1 = (title, style) => {
-    return `<h1 class="${style} ${pass_fail}">${title}</h1>`
-}
+// const h1 = (title, style) => {
+//     return `<h1 class="${style} ${pass_fail}">${title}</h1>`
+// }
 
 const student = (name, classroom, info) => `
     <div id="student">
-        ${h1(name, "xx-large")}
+        ${h1(name, pass_fail)}
         ${section(classroom, "section--padded")}
         ${aside(info, "pushRight")}
     </div>
 `
+
+//challenge
+const h1 = (...props) => {
+    console.log(props)
+    return `<h1 class="${props[1]}">${props[0]}</h1>`
+  }
+
+const section = (...props) => {
+    console.log(props)
+    return `<section class="bordered dashed ${props[1]}">${props[0]}</section>`
+}
+
+const aside = (...props) => {
+    console.log(props)
+    return `<aside class="${props[1]}">${props[0]}</aside>`
+}
+
+const element = (...props) => {
+    //name of html element
+    let elementName = props[0];
+    //name of html content(innerhtml)
+    let elementInfo = props[1];
+    //class name
+    let className = props[2];
+    const resultingElement = document.createElement(elementName);
+    resultingElement.className = className
+    resultingElement.textContent = elementInfo
+    return resultingElement;
+}
+
+const studentHTML = (...props) => {
+    let pass_or_fail = props[0];
+    let student = props[1];
+    let div = element('div', '', 'student')
+    let h1 = element('h1', student.name, pass_or_fail)
+    let section = element('section', student.class, 'bordered dashed section--padded')
+    let aside = element('aside', student.info, 'pushRight')
+    div.appendChild(h1);
+    div.appendChild(section);
+    div.appendChild(aside);
+    console.log(div)
+    return div;
+}
 
 const articleContainer = document.querySelector("#container");
 
 for (let i = 0; i < students.length; i++) {
     if (students[i].score >= 60) {
         console.log(students[i].name + " is passing")
-        pass_fail = "passing"
-        articleContainer.innerHTML += student(students[i].name, students[i].class, students[i].info)
+        pass_fail = 'passing'
+        let div = studentHTML(pass_fail, students[i])
+        articleContainer.appendChild(div);
     } else {
         console.log(students[i].name + " is failing")
-        pass_fail = "failing"
-        articleContainer.innerHTML += student(students[i].name, students[i].class, students[i].info)
+        pass_fail = 'failing'
+        let div = studentHTML(pass_fail, students[i])
+        articleContainer.appendChild(div);
     }
 }
 
+// for (let i = 0; i < students.length; i++) {
+//     if (students[i].score >= 60) {
+//         console.log(students[i].name + " is passing")
+//         pass_fail = "passing"
+//         articleContainer.innerHTML += student(students[i].name, students[i].class, students[i].info)
+//     } else {
+//         console.log(students[i].name + " is failing")
+//         pass_fail = "failing"
+//         articleContainer.innerHTML += student(students[i].name, students[i].class, students[i].info)
+//     }
+// }
+
 //joe's way
+
+// const h1 = (title, style) => {
+//     return `<h1 class="${style}">${title}</h1>`
+// }
 
 // const student = (name, classroom, info, status) => `
 //     <div id="student">
@@ -127,7 +188,3 @@ for (let i = 0; i < students.length; i++) {
 //     document.querySelector("#container").innerHTML += 
 //     `${studentComponent}`
 // })
-
-// const h1 = (title, style) => {
-//     return `<h1 class="${style}">${title}</h1>`
-// }
