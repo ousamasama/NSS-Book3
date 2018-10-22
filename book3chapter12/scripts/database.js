1d0da0fbc9c4892eaa026ce312d06333e07c436c
Ousama Elayan <ousamasama@gmail.com>
2018-10-22 14:17:31 -0500

    added book3chapter12

---
+++ book3chapter12/scripts/database.js
@@ -0,0 +1,38 @@
+/*
+    Purpose:
+    This file contains all code related to manipulating,
+    or accessing the main flower database. Code not serving
+    this purpose should not be added to this file.
+*/
+
+const FlowerDatabase = [
+    {
+      "id": 1,
+      "commonName": "Rose",
+      "species": ["Hulthemia", "Hesperrhodos", "Platyrhodon", "Gymnocarpae"]
+    },
+    {
+      "id": 2,
+      "commonName": "Daisy",
+      "species": ["Aster breweri", "Aster chezuensis", "Aster protoflorian", "Gymnocarpae"]
+    }
+];
+
+const getFlower = function (id) {
+    return FlowerDatabase.find(flower => flower.id === id)
+}
+
+const saveFlower = function (flower) {
+    // Verify that the state of the flower meets requirements
+    if (
+        flower.commonName.length > 0
+        && flower.hasOwnProperty("id")
+        && flower.hasOwnProperty("commonName")
+        && flower.hasOwnProperty("species")
+        && (FlowerDatabase.find(f => f.id === flower.id) || null) === null
+    ) {
+        FlowerDatabase.push(flower)
+    } else {
+        throw "Flower not saved. Missing, or incorrect, required properties."
+    }
+}
